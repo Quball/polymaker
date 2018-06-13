f42166c7a87680ff1e486b2e39656e993bf884d6
Thomas Nielsen <tsn1985@gmail.com>
2018-06-13 23:02:51 +0200

    Draw features to map

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-console.log("Hello World")
+import axios from "axios";
+import L from "leaflet";
+
+const app = () => {
+  axios.get("http://localhost:3000/api/polygons").then(res => {
+    console.log(res.data);
+    createPolygons(res.data);
+  });
+};
+
+const createPolygons = polygons => {
+  let map = L.map("mapid").setView([51.505, -0.09], 13);
+  L.geoJson(polygons, {
+    style: (feature, key) => {
+      return {color: feature.properties.color || "#000"}
+    }
+  })
+  .bindPopup(layer => {
+    return layer.feature.id
+  })
+  .addTo(map);
+};
+
+app();
